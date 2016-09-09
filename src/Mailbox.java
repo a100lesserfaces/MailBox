64f1afe567e141548e773292b7c1207ba0926420
Alexandria BenDebba <abendebba@lgsinnovations.com>
2016-09-09 15:25:11 -0400

    Changing the layout for better visibility

--- src/Mailbox.java
+++ src/Mailbox.java
@@ -1,9 +1,5 @@
 import org.eclipse.swt.*;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.graphics.*;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.*;
 
 import java.io.*;
@@ -17,34 +13,31 @@ public class Mailbox {
 		Shell my_shell = new Shell(my_display);
 		my_shell.setText("Send a letter to Santa");
 		my_shell.setBounds(320, 320, 420, 320);
-		GridLayout grid_layout = new GridLayout();
-		grid_layout.numColumns = 2;
-		my_shell.setLayout(grid_layout);
-		GridData grid_data = new GridData(SWT.HORIZONTAL);
+		my_shell.setLayout(new FillLayout());
 		Label label = new Label(my_shell, SWT.WRAP | SWT.CENTER | SWT.LEFT);
 		label.setText("Outline your Present by manually selecting waveforms.");
 		Listener listener =  new SelectWaveform();
 		label.addListener(SWT.MouseDoubleClick, listener);
-		grid_data.horizontalSpan = 2;
-		label.setLayoutData(grid_data);
-		// Waveform selection
+		
 		String location = System.getenv("SDRROOT") + "//dom//waveforms";
 		File waveforms_folder = new File(location);
-		final Tree tree = new Tree(my_shell, SWT.RIGHT);
+		final Tree tree = new Tree(my_shell, 0);
+		TreeItem placeholder = new TreeItem(tree, 0);
+		placeholder.setText("placeholder");
 		for (File file_entry : waveforms_folder.listFiles()) {
-			TreeItem topLevel = new TreeItem(tree, SWT.RIGHT);
+			TreeItem topLevel = new TreeItem(tree, 0);
 			topLevel.setText(file_entry.getName());
 			// working on the nested
 			if (file_entry.getName().equals("rh")) {
 				for (File file_entry2 : file_entry.listFiles()) {
-					TreeItem lowerLevel = new TreeItem(topLevel,SWT.RIGHT);
+					TreeItem lowerLevel = new TreeItem(topLevel,0);
 					lowerLevel.setText(file_entry2.getName());
 				}
 			}
 		}
-		// end the tree
+		
 		// Selected waveforms
-		final Tree selectedWaveforms = new Tree(my_shell, SWT.RIGHT);
+		final Tree selectedWaveforms = new Tree(my_shell, 0);
 		Button add_waveform = new Button(my_shell, 0);
 		add_waveform.setText("Add >>");
 		add_waveform.addListener(SWT.MouseDown, new Listener(){
@@ -55,6 +48,7 @@ public class Mailbox {
 				for (int i = 0; i < selection.length; i++) {
 					TreeItem item = new TreeItem(selectedWaveforms, 0);
 					item.setText(selection[i].getText());
+					waveforms.add(selection[i].getText());
 				}
 			}
 			
