fa2c8b56838fc020bafb639994d54960aca0435c
Alexandria BenDebba <abendebba@lgsinnovations.com>
2016-09-09 14:38:02 -0400

    Adding waveform paths when double clicking and adding better listeners

--- src/Mailbox.java
+++ src/Mailbox.java
@@ -7,66 +7,63 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.*;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class Mailbox {
 
 	public static void main(String[] args) throws IOException {
-		Display myDisplay = new Display();
-		Shell myShell = new Shell(myDisplay);
-		myShell.setText("Send a letter to Santa");
-	    myShell.setBounds(320, 320, 420, 320);
-		GridLayout gridLayout = new GridLayout();
-		gridLayout.numColumns = 2;
-		myShell.setLayout(gridLayout);
-		GridData gridData = new GridData(SWT.HORIZONTAL);
-		
-		Label label = new Label(myShell, SWT.WRAP | SWT.CENTER | SWT.LEFT);
+		final ArrayList<String> waveforms = new ArrayList<String>();
+		Display my_display = new Display();
+		Shell my_shell = new Shell(my_display);
+		my_shell.setText("Send a letter to Santa");
+		my_shell.setBounds(320, 320, 420, 320);
+		GridLayout grid_layout = new GridLayout();
+		grid_layout.numColumns = 2;
+		my_shell.setLayout(grid_layout);
+		GridData grid_data = new GridData(SWT.HORIZONTAL);
+		Label label = new Label(my_shell, SWT.WRAP | SWT.CENTER | SWT.LEFT);
 		label.setText("Outline your Present by manually selecting waveforms.");
-		gridData.horizontalSpan = 2;
-		label.setLayoutData(gridData);
-		
-		// Selected waveforms
-		final List list = new List(myShell, SWT.RIGHT);
-		
+		Listener listener =  new SelectWaveform();
+		label.addListener(SWT.MouseDoubleClick, listener);
+		grid_data.horizontalSpan = 2;
+		label.setLayoutData(grid_data);
 		// Waveform selection
 		String location = System.getenv("SDRROOT") + "//dom//waveforms";
 		File waveforms_folder = new File(location);
-		final Tree tree = new Tree(myShell, SWT.RIGHT);
-		for (final File fileEntry : waveforms_folder.listFiles()) {
+		final Tree tree = new Tree(my_shell, SWT.RIGHT);
+		for (File file_entry : waveforms_folder.listFiles()) {
 			TreeItem topLevel = new TreeItem(tree, SWT.RIGHT);
-			topLevel.setText(fileEntry.getName());
-			topLevel.addListener(SWT.MouseDoubleClick, new Listener(){
-
-				@Override
-				public void handleEvent(Event event) {
-					System.out.println("double click");
-					list.add(fileEntry.getName());
-					
-				}
-				
-			});
+			topLevel.setText(file_entry.getName());
 			// working on the nested
-			if (fileEntry.getName().equals("rh")) {
-				for (final File fileEntry2 : fileEntry.listFiles()) {
+			if (file_entry.getName().equals("rh")) {
+				for (File file_entry2 : file_entry.listFiles()) {
 					TreeItem lowerLevel = new TreeItem(topLevel,SWT.RIGHT);
-					lowerLevel.setText(fileEntry2.getName());
-					lowerLevel.addListener(SWT.MouseDoubleClick, new Listener(){
-						@Override
-						public void handleEvent(Event event) {
-							System.out.println("double click");
-							list.add(fileEntry.getName());
-						}
-					});
+					lowerLevel.setText(file_entry2.getName());
 				}
 			}
 		}
 		// end the tree
-		
-		myShell.open();
-		while (!myShell.isDisposed()) {
-			if (!myDisplay.readAndDispatch()) myDisplay.sleep();
+		// Selected waveforms
+		final Tree selectedWaveforms = new Tree(my_shell, SWT.RIGHT);
+		Button add_waveform = new Button(my_shell, 0);
+		add_waveform.setText("Add >>");
+		add_waveform.addListener(SWT.MouseDown, new Listener(){
+
+			@Override
+			public void handleEvent(Event event) {
+				TreeItem[] selection = tree.getSelection();
+				for (int i = 0; i < selection.length; i++) {
+					TreeItem item = new TreeItem(selectedWaveforms, 0);
+					item.setText(selection[i].getText());
+				}
+			}
+			
+		});
+		my_shell.open();
+		while (!my_shell.isDisposed()) {
+			if (!my_display.readAndDispatch()) my_display.sleep();
 		}
-		myDisplay.dispose();
+		my_display.dispose();
 
 	}
 	
